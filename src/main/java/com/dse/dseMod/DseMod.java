99ec24ed3deac50e98af52d169eb3272ce017d02
nahanahaha-legend <super.perl.user@gmail.com>
2018-10-07 23:11:10 +0900

    version 0.2.0.1

--- src/main/java/com/dse/dseMod/DseMod.java
+++ src/main/java/com/dse/dseMod/DseMod.java
@@ -32,7 +32,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @EventBusSubscriber
 public class DseMod {
 	public static final String  MODID = "dsemod";
-	public static final String  VERSION = "0.2.0.1";
+	public static final String  VERSION = "0.2.0.2";
 
 	// mamedol holder
 	public static class MAMEDOLS {
