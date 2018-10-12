3234990620b287d6335068068ca48ebbbb107719
nahanahaha-legend <super.perl.user@gmail.com>
2018-10-13 02:45:14 +0900

    version 0.2.0.4 add dse rail

--- src/main/java/com/dse/dseMod/DseMod.java
+++ src/main/java/com/dse/dseMod/DseMod.java
@@ -34,7 +34,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @EventBusSubscriber
 public class DseMod {
 	public static final String  MODID = "dsemod";
-	public static final String  VERSION = "0.2.0.3";
+	public static final String  VERSION = "0.2.0.4";
 
 	// mamedol holder
 	public static class MAMEDOLS {
