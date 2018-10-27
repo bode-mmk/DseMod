0d14d2adcc542707e51de7ad84b5ad90ea8deb42
nahanahaha-legend <super.perl.user@gmail.com>
2018-10-27 15:46:36 +0900

    add bonbori state

--- src/main/java/com/dse/dseMod/block/BlockBonbori.java
+++ src/main/java/com/dse/dseMod/block/BlockBonbori.java
@@ -4,22 +4,27 @@ import com.dse.dseMod.DseMod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 public class BlockBonbori extends Block {
+	public static final PropertyEnum<LightMode> LIGHT_MODE = PropertyEnum.<LightMode>create("light_mode", LightMode.class);
 	public static final AxisAlignedBB FROG_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.6D, 1.0D);
 
 	public BlockBonbori() {
 		super(Material.WOOD);
 		this.setHardness(0.1f);
 		this.setCreativeTab(CreativeTabs.DECORATIONS);
-		//this.setDefaultState(this.getStateFromMeta(0));
+		this.setDefaultState(this.getStateFromMeta(0));
 		this.setUnlocalizedName("bonbori");
 		this.setRegistryName("bonbori");
 	}
@@ -29,20 +34,17 @@ public class BlockBonbori extends Block {
 		return FROG_AABB;
 	}
 
-	/*
 	@Override
 	public IBlockState getStateFromMeta(final int meta) {
-		IBlockState blockstate = super.getStateFromMeta(meta & 3);
-		blockstate.withProperty(POWER, metaToPower(meta));
-		return blockstate;
+		return this.blockState.getBaseState().withProperty(LIGHT_MODE, LightMode.byMetadata(meta));
 	}
 
 	@Override
 	public int getMetaFromState(final IBlockState state) {
-		return ((EnumFacing)state.getValue(DIRECTION)).getHorizontalIndex() + powerToMeta(((Integer)state.getValue(POWER)).intValue());
+		return LightMode.metaFromLightMode(state.getValue(LIGHT_MODE));
 	}
 
-
+	/*
 	@Override
 	public boolean hasTileEntity(IBlockState state) {
 		return true;
@@ -81,9 +83,62 @@ public class BlockBonbori extends Block {
 		drops.add(new ItemStack(DseMod.BLOCK_ITEMS.bonbori_tile_item, 1, 0));
 	}
 
-	/*
 	@Override
 	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, new IProperty[] {POWER, DIRECTION});
-	}*/
+		return new BlockStateContainer(this, new IProperty[] {LIGHT_MODE});
+	}
+
+	public static enum LightMode implements IStringSerializable
+	{
+		ON("on"),
+		OFF("off"),
+		AUTO_ON("auto_on"),
+		AUTO_OFF("auto_off");
+
+		private String mode;
+
+		private LightMode(String mode) {
+			this.mode = mode;
+		}
+
+		public static LightMode byMetadata(int meta)
+		{
+			if(meta == 0) {
+				return LightMode.OFF;
+			}else if(meta == 1) {
+				return LightMode.ON;
+			}else if(meta == 2){
+				return LightMode.AUTO_OFF;
+			}else {
+				return LightMode.AUTO_ON;
+			}
+		}
+
+		public static int metaFromLightMode(LightMode mode) {
+			if(mode == LightMode.OFF) {
+				return 0;
+			}else if(mode == LightMode.ON) {
+				return 1;
+			}else if(mode == LightMode.AUTO_OFF){
+				return 2;
+			}else {
+				return 3;
+			}
+		}
+
+		public static LightMode toggleLightMode(LightMode mode) {
+			if(mode == LightMode.OFF) {
+				return LightMode.ON;
+			}else if(mode == LightMode.ON) {
+				return LightMode.AUTO_OFF;
+			}else {
+				return LightMode.OFF;
+			}
+		}
+
+		@Override
+		public String getName() {
+			return mode;
+		}
+	}
 }
---
+++ src/main/java/com/dse/dseMod/tileentity/TileEntityBonbori.java
@@ -0,0 +1,13 @@
+package com.dse.dseMod.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+
+public class TileEntityBonbori extends TileEntity implements ITickable {
+
+	@Override
+	public void update() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+}
