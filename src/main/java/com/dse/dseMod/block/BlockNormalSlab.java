c4260cc3fb87298c55fcdf3e899cc6e64643a600
nahanahaha-legend <super.perl.user@gmail.com>
2018-11-05 23:51:13 +0900

    add normal slab

---
+++ src/main/java/com/dse/dseMod/block/BlockNormalSlab.java
@@ -0,0 +1,79 @@
+package com.dse.dseMod.block;
+
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public abstract class BlockNormalSlab extends BlockSlab {
+	public BlockNormalSlab() {
+		super(Material.WOOD);
+		IBlockState iblockstate = this.blockState.getBaseState();
+
+		if(!this.isDouble()) {
+			iblockstate.withProperty(HALF, BlockSlab.EnumBlockHalf.BOTTOM);
+		}
+
+		this.setDefaultState(iblockstate);
+		this.useNeighborBrightness = !this.isDouble();
+	}
+
+	@Override
+	public String getUnlocalizedName(int meta) {
+		// TODO 自動生成されたメソッド・スタブ
+		return this.getUnlocalizedName();
+	}
+
+	@Override
+	public boolean isDouble() {
+		// TODO 自動生成されたメソッド・スタブ
+		return true;
+	}
+
+	@Override
+	public final IBlockState getStateFromMeta(final int meta) {
+		IBlockState blockstate = this.blockState.getBaseState();
+		if(!this.isDouble()) {
+			blockstate = blockstate.withProperty(HALF, ((meta&8)!=0)?EnumBlockHalf.TOP:EnumBlockHalf.BOTTOM);
+		}
+
+		return blockstate;
+	}
+
+	@Override
+	public final int getMetaFromState(final IBlockState state) {
+		int meta = 0;
+
+		if(!this.isDouble()&& state.getValue(HALF)==EnumBlockHalf.TOP) {
+			meta |= 8;
+		}
+
+		return meta;
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		if(!this.isDouble()){
+			return new BlockStateContainer(this, new IProperty[] {HALF});
+		}
+		return new BlockStateContainer(this, new IProperty[] {});
+	}
+
+	//@Override
+    //public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state)
+    //{
+    //}
+
+	@Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+    {
+        IBlockState state = super.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
+
+        return state;
+    }
+}
