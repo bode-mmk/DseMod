d3d05e943ffc2cd3a0ab087c744e51bfed634da0
nahanahaha-legend <super.perl.user@gmail.com>
2019-06-22 20:17:02 +0900

    v0.2.1.2

--- src/main/java/com/dse/dseMod/DseMod.java
+++ src/main/java/com/dse/dseMod/DseMod.java
@@ -62,7 +62,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 @EventBusSubscriber
 public class DseMod {
 	public static final String  MODID = "dsemod";
-	public static final String  VERSION = "0.2.1.1";
+	public static final String  VERSION = "0.2.1.2";
 	// world generator holder
 	//public static class WORLD_GEN {
 	//	public static IWorldGenerator dse_flower_generator = new com.dse.dseMod.worldgen.WorldGenDseFlower();
